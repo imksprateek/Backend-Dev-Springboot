9df081612438d94e2e84ae63afffe245a35803b7
imksprateek <ksprateek2004@gmail.com>
2024-05-26 20:52:36 +0530

    3 fields added -Thymeleaf validation

--- 06-spring-boot-spring-mvc/02-validation-demo/validationdemo/src/main/java/com/ksprateek/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-validation-demo/validationdemo/src/main/java/com/ksprateek/springdemo/mvc/Customer.java
@@ -1,14 +1,34 @@
 package com.ksprateek.springdemo.mvc;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
+    //First name is not mandatory
     private String firstName;
 
+    //Make lastName field mandatory and specify size a minimum of 1
     @NotNull(message = "is required")
     @Size(min=1, message = "is required")
     private String lastName = "";
 
+    //Let's define freePasses value to be greater than 0 and less than 10
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value =10, message = "must be less than or equal to 10")
+    private int freePasses;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(@Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    //Getters and setters for the fields
     public String getFirstName() {
         return firstName;
     }
--- 06-spring-boot-spring-mvc/02-validation-demo/validationdemo/src/main/java/com/ksprateek/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/02-validation-demo/validationdemo/src/main/java/com/ksprateek/springdemo/mvc/CustomerController.java
@@ -1,27 +1,49 @@
 package com.ksprateek.springdemo.mvc;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+    //Request mapping for form
     @GetMapping("/")
     public String showForm(Model theModel){
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
     }
 
+    //Request mapping to process the form
     @PostMapping("/processForm")
+    //@ModelAttribute reads the customer information posted by the html form
+    //@Valid performs form validation according to the rules specified in Customer class
+    //BindingResult object theBindingResult is the result of validation
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer
     , BindingResult theBindingResult){
+        //Print statement for debugging
+        System.out.println("Last Name: |" + theCustomer.getLastName() + "|");
+        //if validation fails, return the user back to customer-form page. If it succeeds, redirect user to customer-confirmation page
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }else{
             return "customer-confirmation";
         }
+    }
+
+    //Use @InitBinder to pre-process requests
+    //For every new request that comes to our controller, @InitBinder code will execute first
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        //this initBinder method pre-processes every String form data, removes leading and trailing white space. If String has only white space, trim it to null.
+//        StringTrimmerEditor is defined in Spring API to remove leading and trailing whitespaces
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+//        For every string class apply stringTrimmerEditor:
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
