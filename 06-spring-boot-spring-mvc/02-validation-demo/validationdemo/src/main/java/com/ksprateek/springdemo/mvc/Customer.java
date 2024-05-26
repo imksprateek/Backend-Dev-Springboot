1274e91a77efe2ac20f9027e232a7ee27415a0b0
imksprateek <ksprateek2004@gmail.com>
2024-05-26 21:17:29 +0530

    Regular expression added for the new field Postal code, made fields Free passes and Postal code mandatory

--- 06-spring-boot-spring-mvc/02-validation-demo/validationdemo/src/main/java/com/ksprateek/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-validation-demo/validationdemo/src/main/java/com/ksprateek/springdemo/mvc/Customer.java
@@ -1,8 +1,5 @@
 package com.ksprateek.springdemo.mvc;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     //First name is not mandatory
@@ -14,17 +11,31 @@ public class Customer {
     private String lastName = "";
 
     //Let's define freePasses value to be greater than 0 and less than 10
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value =10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
-    @Min(value = 0, message = "must be greater than or equal to zero")
-    @Max(value = 10, message = "must be less than or equal to 10")
-    public int getFreePasses() {
+    //In the regular expression below, '^' indicates start of the string, '[a-zA-Z0-9]' indicates it contains a-z or A-Z or 0-9, '{5}' indicates the length should be 5, '$' indicates end of the string
+    @NotNull(message = "is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}$", message = "only 5 chars/digits")
+    private String postalCode;
+
+
+//    Getters and Setters
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(@Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
